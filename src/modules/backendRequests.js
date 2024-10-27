f32d9a1f5f0a7095ff3ecd61de34cce9eed1e270
Abraham-Niebla <a1176033@uabc.edu.mx>
2024-10-26 20:08:32 -0700

    Cambios visuales mínimos y de backend url

--- src/modules/backendRequests.js
+++ src/modules/backendRequests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backendPath = 'http://localhost:5000/api/text-to-speech';
+const backendPath = 'http://127.0.0.1:5000/api/text-to-speech';
 
 async function sendBackend(msj){
   try {
