c422c9c4ea22dda0bef961f48f9bdfbb59e9a186
Abraham-Niebla <a1176033@uabc.edu.mx>
2024-11-15 19:22:26 -0800

    Cambios de estilo de alertadores

--- src/Components/toastContainers/toastContainers.js
+++ src/Components/toastContainers/toastContainers.js
@@ -5,7 +5,7 @@ import "./toastContainers.css"; // Importa tus estilos personalizados
 
 const CustomCloseButton = ({ closeToast }) => (
     <button onClick={closeToast} className="close-button">
-        ✖️
+        X
     </button>
 );
 
@@ -15,6 +15,7 @@ async function error(msg) {
         className: "toast-error", // Aplica la clase personalizada para errores
         progressClassName: "toast-progress-bar-error", // Aplica la clase personalizada para la barra de progreso de errores
         closeButton: <CustomCloseButton />, // Usa el botón de cierre personalizado
+        icon: false, // Elimina el icono del toast
     });
 }
 
@@ -24,6 +25,7 @@ async function success(msg) {
         className: "toast-success", // Aplica la clase personalizada
         progressClassName: "toast-progress-bar-success", // Aplica la clase personalizada para la barra de progreso
         closeButton: <CustomCloseButton />, // Usa el botón de cierre personalizado
+        icon: false, // Elimina el icono del toast
     });
 }
 
@@ -33,6 +35,7 @@ async function alert(msg) {
         className: "toast-alert", // Aplica la clase personalizada
         progressClassName: "toast-progress-bar-alert", // Aplica la clase personalizada para la barra de progreso
         closeButton: <CustomCloseButton />, // Usa el botón de cierre personalizado
+        icon: false, // Elimina el icono del toast
     });
 }
 
