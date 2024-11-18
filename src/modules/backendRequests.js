361bf1b96a8dfc25c182e85f8b8a9cfc50603e8d
Abraham-Niebla <a1176033@uabc.edu.mx>
2024-11-17 22:00:17 -0800

    Corrección de error (ya se pueden enviar mensajes con sesión de Google)

--- src/modules/backendRequests.js
+++ src/modules/backendRequests.js
@@ -2,21 +2,23 @@ import axios from "axios";
 
 const backendPath = "http://127.0.0.1:5000/api/text-to-speech";
 
-async function sendBackend(msj) {
-  try {
-    const response = await axios.post(backendPath, {
-      input_text: msj,
-    });
-    const data = await response.data;
-    const audioB64 = data.audio_file;
+async function sendBackend(msj, voice_model, ia_model) {
+    try {
+        const response = await axios.post(backendPath, {
+            input_text: msj,
+            voice_model: voice_model,
+            ia_model: ia_model,
+        });
+        const data = await response.data;
+        const audioB64 = data.audio_file;
 
-    return {
-      audio: audioB64,
-      text: data.input_text,
-    };
-  } catch (error) {
-    console.error("Error al enviar la petición:", error);
-  }
+        return {
+            audio: audioB64,
+            text: data.input_text,
+        };
+    } catch (error) {
+        console.error("Error al enviar la petición:", error);
+    }
 }
 
 export { sendBackend };
